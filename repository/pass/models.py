7f0032965e575fc3b20ecb65e2861fe5e09a447e
Aligh1307 <gharahlou@gmail.com>
2023-08-26 10:58:04 +0330

    add space to models.py

--- repository/pass/models.py
+++ repository/pass/models.py
@@ -10,7 +10,8 @@ class Password(models.Model):
     name = models.CharField(max_length=250, verbose_name='نام')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users', verbose_name='کاربر')
     password = models.CharField(max_length=250, verbose_name='رمز عبور')
-    description = models.TextField(verbose_name='توضیحات')
+    description = models.TextField(verbose_name='توضیحات', null=True, blank=True)
 
     def __str__(self):
-        return self.password
+        return self.name
+
--- repository/pass/views.py
+++ repository/pass/views.py
@@ -13,11 +13,12 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class PasswordViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
-        return Password.objects.all()
+        return Password.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
     serializer_class = PasswordSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.SearchFilter]
-    search_fields = ['password', 'description']
-
-
+    search_fields = ['password', 'description', 'name']
